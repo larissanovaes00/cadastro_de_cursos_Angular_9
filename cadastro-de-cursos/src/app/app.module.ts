a654323ac8d88086de1d546664288c6d2b15afa1
larissanovaes00 <larissanovaes00@hotmail.com>
2020-11-21 12:07:57 -0300

    initial files and html structure

--- cadastro-de-cursos/src/app/app.module.ts
+++ cadastro-de-cursos/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CoursesListComponent } from './components/courses-list/courses-list.component';
+import { CreateCourseComponent } from './components/create-course/create-course.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CoursesListComponent,
+    CreateCourseComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
